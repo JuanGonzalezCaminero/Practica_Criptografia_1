c4c05d33acd342bb970f9d052b04a8da0225edd0
Juan <juangonzalez5@hotmail.es>
2018-11-02 19:40:23 +0100

    Bit stream encoding implemented

--- Main.py
+++ Main.py
@@ -1,20 +1,41 @@
 from  SeparatePrograms.SumaMultiplicacion import *
 from bitstring import *
 import functools
+from SeparatePrograms.encode import *
+import sys
 
-result = 0
-add_prod = int(input("¿Quieres sumar o multiplicar?[0/1]\n"))
-if add_prod == 0:
-    elements = input("Introduce los elementos a sumar, separados por comas:\n")
-    elements = elements.split(",")
-    elements = [BitArray(bin="0b" + e) for e in elements]
-    result = functools.reduce(lambda a,b : add(a, b), elements)
-if add_prod == 1:
-    elements = input("Introduce los elementos a multiplicar, separados por comas:\n")
-    elements = elements.split(",")
-    elements = [BitArray(bin = "0b" + e) for e in elements]
-    irreducible_polynomial = input("introduce el polinomio irreducible:\n")
-    irreducible_polynomial = BitArray(bin = "0b" + irreducible_polynomial)
-    result = functools.reduce(lambda a,b : product_in_field(a, b, irreducible_polynomial), elements)
+def pruebaSumMult():
+    result = 0
+    add_prod = int(input("¿Quieres sumar o multiplicar?[0/1]\n"))
+    if add_prod == 0:
+        elements = input("Introduce los elementos a sumar, separados por comas:\n")
+        elements = elements.split(",")
+        elements = [BitArray(bin="0b" + e) for e in elements]
+        result = functools.reduce(lambda a,b : add(a, b), elements)
+    if add_prod == 1:
+        elements = input("Introduce los elementos a multiplicar, separados por comas:\n")
+        elements = elements.split(",")
+        elements = [BitArray(bin = "0b" + e) for e in elements]
+        irreducible_polynomial = input("introduce el polinomio irreducible:\n")
+        irreducible_polynomial = BitArray(bin = "0b" + irreducible_polynomial)
+        result = functools.reduce(lambda a,b : product_in_field(a, b, irreducible_polynomial), elements)
+    print(result.bin)
 
-print(result.bin)
+def pruebaCodificacion(filename):
+    file = open(sys.path[0] + filename, 'rb+')
+
+    encoder = Encoder([[BitArray("0b0010"), BitArray("0b0110"), BitArray("0b0001"), BitArray("0b0000"), BitArray("0b0000"), BitArray("0b0000")],
+                   [BitArray("0b0110"), BitArray("0b1110"), BitArray("0b0000"), BitArray("0b0001"), BitArray("0b0000"), BitArray("0b0000")],
+                   [BitArray("0b0110"), BitArray("0b1110"), BitArray("0b0000"), BitArray("0b0000"), BitArray("0b0001"), BitArray("0b0000")],
+                   [BitArray("0b1110"), BitArray("0b1101"), BitArray("0b0000"), BitArray("0b0000"), BitArray("0b0000"), BitArray("0b0001")]],
+                  BitArray("0b10011"))
+
+    encoded_file = encoder.encode_stream(file,)
+    #print(encoded_file.bin)
+    return encoded_file
+
+#pruebaSumMult()
+encoded = pruebaCodificacion("\\Text.txt")
+file = open(sys.path[0] + "\\EncodedText.txt", 'wb+')
+file.write(bytes.fromhex(encoded.hex))
+
--- SeparatePrograms/encode.py
+++ SeparatePrograms/encode.py
@@ -6,48 +6,81 @@ from SeparatePrograms.SumaMultiplicacion import *
 #(que es la matriz de la aplicación lineal entre F16 y el código)
 #https://en.wikipedia.org/wiki/Linear_map for further reference
 
-#This module contains utility methods for encoding elements from a finite field
+#This class contains utility methods for encoding elements from a finite field
 #using the transformation matrix provided and the irreducible polynomial with which the
 #field was generated, (everything in Z2)
 
-def encode_element(element, transformation_matrix, irreducible_polynomial):
-    '''
-    Returns the encoding of the element of the finite field given over Z2 using the
-    given irreducible polynomial, (Its a linear subspace of Z2) the linear map (Aplicación lineal),
-    which maps elements from the field onto elements of the code, is given by its transformation matrix
-    :param element: The element of a finite field to be encoded
-    :param transformation_matrix: The transformation matrix of the linear mapping from the field onto the code
-    :param irreducible_polynomial: The irreducible polynomial which gives a finite field over Z2
-    :return: The encoding of the element by the linear mapping given by the transformation matrix
-    '''
-    #The result will have a length (in bits) of n * d, where n is the number of
-    #rows in the transformation matrix and d, the degree of the irreducible
-    #polynomial (The length of the elements in the field it generates)
-    irreducible_degree = len(irreducible_polynomial) - 1
-    code_element_length = len(transformation_matrix[0])
-    result = BitArray()
+class Encoder:
+    def __init__(self, transformation_matrix, irreducible_polynomial):
+        '''
+        :param transformation_matrix: The transformation matrix of the linear mapping from the field onto the code
+        :param irreducible_polynomial: The irreducible polynomial which gives a finite field over Z2
+        '''
+        self.transformation_matrix = transformation_matrix
+        self.irreducible_polynomial = irreducible_polynomial
 
-    #The accesses to the matrix fields are performed top to bottom and left to right, maybe
-    #that could be avoided for memory-access efficiency (this defeats the locality of reference)
-    for column in range(code_element_length):
+    def encode_element(self, element):
+        '''
+        Returns the encoding of the element of the finite field given over Z2 using the
+        given irreducible polynomial, (Its a linear subspace of Z2) the linear map (Aplicación lineal),
+        which maps elements from the field onto elements of the code, is given by its transformation matrix
+        :param element: The element of a finite field to be encoded
+        :return: The encoding of the element by the linear mapping given by the transformation matrix, in a
+        bitstring.BitArray object
+        '''
+        #The result will have a length (in bits) of n * d, where n is the number of
+        #rows in the transformation matrix and d, the degree of the irreducible
+        #polynomial (The length of the elements in the field it generates)
+        irreducible_degree = len(self.irreducible_polynomial) - 1
+        code_element_length = len(self.transformation_matrix[0])
+        result = BitArray()
 
-        element_to_append = BitArray(irreducible_degree)
+        #The accesses to the matrix fields are performed top to bottom and left to right, maybe
+        #that could be avoided for memory-access efficiency (this defeats the locality of reference)
+        for column in range(code_element_length):
 
-        for row in range(len(transformation_matrix)):
+            element_to_append = BitArray(irreducible_degree)
 
-            element_to_append = add(element_to_append,
-                product_in_field(element[row : row+1], transformation_matrix[row][column], irreducible_polynomial))
+            for row in range(len(self.transformation_matrix)):
 
-        result.append(element_to_append)
+                element_to_append = add(element_to_append,
+                    product_in_field(element[row : row+1], self.transformation_matrix[row][column], self.irreducible_polynomial))
 
-    return result
+            result.append(element_to_append)
 
-print(encode_element(BitArray("0b1010"),
-                     [[BitArray("0b0010"),BitArray("0b0110"),BitArray("0b0001"),BitArray("0b0000"),BitArray("0b0000"),BitArray("0b0000")],
-                     [BitArray("0b0110"),BitArray("0b1110"),BitArray("0b0000"),BitArray("0b0001"),BitArray("0b0000"),BitArray("0b0000")],
-                     [BitArray("0b0110"),BitArray("0b1110"),BitArray("0b0000"),BitArray("0b0000"),BitArray("0b0001"),BitArray("0b0000")],
-                     [BitArray("0b1110"),BitArray("0b1101"),BitArray("0b0000"),BitArray("0b0000"),BitArray("0b0000"),BitArray("0b0001")]],
-                     BitArray("0b10011")).bin)
+        #print("Received: ", element.bin)
+        #print("Encoding: ", result.bin)
+
+        return result
+
+    def encode_stream(self, bit_stream):
+        '''
+        Encodes the provided stream in chunks of n bits where n is the degree of the irreducible
+        polynomial, (and thus, the length of elements in the finite field it generates)
+        :param bit_stream: A file open in binary mode
+        :return: The encoding of the bit stream, in a bitstring.BitArray object
+        '''
+        encoded_data = BitArray()
+        #Read the file and store it here, this should be changed so as not to read the whole file in one operation
+        #but do it in parts
+        data = bit_stream.read()
+        data = BitArray(hex = data.hex())
+        element_length = len(self.irreducible_polynomial) - 1
+        #Choosing the chunks in this way will crop the ending if the length of the data is not a multiple
+        #of the length of the elements, should pre-process the data to avoid this
+        while len(data) > 0:
+            chunk = data[0:element_length]
+            data = data[element_length:]
+            encoded_data.append(self.encode_element(chunk))
+        return encoded_data
+
+
+#print(encode_element(BitArray("0b1010"),
+#                     [[BitArray("0b0010"),BitArray("0b0110"),BitArray("0b0001"),BitArray("0b0000"),BitArray("0b0000"),BitArray("0b0000")],
+#                     [BitArray("0b0110"),BitArray("0b1110"),BitArray("0b0000"),BitArray("0b0001"),BitArray("0b0000"),BitArray("0b0000")],
+#                     [BitArray("0b0110"),BitArray("0b1110"),BitArray("0b0000"),BitArray("0b0000"),BitArray("0b0001"),BitArray("0b0000")],
+#                     [BitArray("0b1110"),BitArray("0b1101"),BitArray("0b0000"),BitArray("0b0000"),BitArray("0b0000"),BitArray("0b0001")]],
+#                     BitArray("0b10011")).bin)
 
 #[[BitArray("0b0010"),BitArray("0b0110"),BitArray("0b0001"),BitArray("0b0000"),BitArray("0b0000"),BitArray("0b0000")],
 # [BitArray("0b0110"),BitArray("0b1110"),BitArray("0b0000"),BitArray("0b0001"),BitArray("0b0000"),BitArray("0b0000")],
